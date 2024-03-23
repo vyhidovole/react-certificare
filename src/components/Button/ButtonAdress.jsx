7b9f616dcb3de511972459d9c8def72f1cb03a78
oleg <vyhidovoleg@gmail.com>
2024-03-23 16:31:59 +0300

    change name of App

--- src/components/Button/ButtonAdress.jsx
+++ src/components/Button/ButtonAdress.jsx
@@ -1,10 +1,11 @@
 import React from "react"
-import App from "./pages/App"
+import ButtonSecond from "../../pages/ButtonSecond"
 
 
+// компонент контейнер для модального окна "доставка"
 export const ButtonAdress=()=>{
     return(
-        <App/>
+       <ButtonSecond/>
     )
 }
 

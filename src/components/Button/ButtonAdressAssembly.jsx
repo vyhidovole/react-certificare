96ee4549de04767e114157ba9c0aa08802aa5e81
oleg <vyhidovoleg@gmail.com>
2024-03-23 16:33:10 +0300

    set up ButtonAssembly

---
+++ src/components/Button/ButtonAdressAssembly.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ButtonSecondAssembly from "../../pages/ButtonSecondAssembly"
+
+
+// компонент контейнер для модального окна "сборка"
+export const ButtonAdressAssembly=()=>{
+    return(
+       <ButtonSecondAssembly/>
+    )
+}
+
+
+

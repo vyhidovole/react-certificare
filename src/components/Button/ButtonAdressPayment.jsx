cff3c72603e7cb5b8465a12eb6622af9821998f9
oleg <vyhidovoleg@gmail.com>
2024-04-05 12:54:29 +0300

    modal corrected

---
+++ src/components/Button/ButtonAdressPayment.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ButtonSecondPayment from "../../pages/ButtonSecondPayment"
+
+
+// компонент контейнер для модального окна "сборка"
+export const ButtonAdressPayment=()=>{
+    return(
+       <ButtonSecondPayment/>
+    )
+}
+
+
+
